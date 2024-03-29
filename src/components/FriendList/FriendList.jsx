80d5f4f861bfd937e1890d866b9c344bf835cafd
Angelina <midnightshout03@gmail.com>
2024-03-29 13:17:22 +0200

    дві задачі

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import FriendListItem from "./FriendListItem"
+export default function FriendList({ friends }) {
+    return (
+        <ul>
+            {friends.map((friend) => (
+                <li key={friend.id}>
+                    <FriendListItem
+                      id={friend.id}
+                        avatar={friend.avatar}
+                        name={friend.name}
+                        isOnline={friend.isOnline}/>
+            </li>
+            ))}
+        </ul>);
+}
+            
